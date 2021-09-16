2b207b96c433c76215d010ec25a87d73faca2270
Cassie Clark <clarkcassie7765@gmail.com>
2021-09-16 16:26:27 -0700

    Adds my new tests - 1 still failing

--- shapes_library/shapes_package/shapes_module.py
+++ shapes_library/shapes_package/shapes_module.py
@@ -6,10 +6,11 @@ class Circle:
     This class implements the Circle API
     """
     def __init__(self, radius: float = None):
-        pass
+        validateCircleRadius(radius)
+        self.radius = radius
 
     def area(self):
-        pass
+        return round(math.pi * self.radius ** 2, 2)
 
 
 class Rectangle:
@@ -17,10 +18,36 @@ class Rectangle:
     This class implements the Rectangle API
     """
     def __init__(self, height: float = None, width: float = None):
-        pass
+        
+        if width is None or width <= 0:    
+                raise ValueError("Supplied width and height must be > 0")
+        self.width = width
+       
+        if height is None or height < 0:
+                raise ValueError("Supplied width and height must be > 0")
+        self.height = height
+
+    
         
     def area(self):
-        pass
+        return round(self.height * self.width, 2)
 
+
+class Capital:
+    """
+    This class implements the Capital API
+    """
+    def __init__(self, name: str = "abcd"):
+        if name[0].islower:
+            raise ValueError("Supplied name must start with capital letter")
+        self.name = name    
+
+
+
+
+
+def validateCircleRadius(radius):
+    if radius is None or radius <= 0:
+        raise ValueError("Supplied radius must be > 0")
 
         
--- tests/shapes_package/test_shapes.py
+++ tests/shapes_package/test_shapes.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 from shapes_library.shapes_package.shapes_module import Circle
 from shapes_library.shapes_package.shapes_module import Rectangle
+from shapes_library.shapes_package.shapes_module import validateCircleRadius
 
 
 class CircleTests(TestCase):
@@ -16,6 +17,20 @@ class CircleTests(TestCase):
         mock_circle = Circle(radius=mock_radius)
         self.assertEqual(mock_circle.area(), expected_area)
 
+    def test_circle_area_raises_ValueError_with_negative_radius(self):
+        """
+        Tests that a circle object with a negative raises a value error
+        """
+        with self.assertRaises(ValueError):
+            mock_circle = Circle(radius = -1.0)
+
+
+    def test_validateCircleRadius_is_handling_errors_properly(self):
+        
+        mock_radius = "string"
+        with self.assertRaises(TypeError):
+            validateCircleRadius(mock_radius)
+
 
 class RectangleTests(TestCase):
     """
@@ -30,4 +45,23 @@ class RectangleTests(TestCase):
         mock_rectangle = Rectangle(height=mock_height, width=mock_width)
         self.assertEqual(mock_rectangle.area(), expected_area)
 
+    def test_rectangle_area_raises_ValueError_with_negative_width_and_height(self):
+        """
+        Tests that a circle object with a negative raises a value error
+        """
+        with self.assertRaises(ValueError):
+            mock_height = Rectangle(height = -1.0)
+            mock_width = Rectangle(width = -1.0)
 
+
+class CapitalTests(TestCase):
+    """
+    Contains all the unit tests for the shapes_module.Name class
+    """
+    def test_name_spelling_starts_with_capital_letter(self):
+       """
+       Tests if first letter of name is Capital
+       """
+       with self.assertRaises(ValueError):
+            mock_name = "Abcd"
+            mock_name[0].islower()
